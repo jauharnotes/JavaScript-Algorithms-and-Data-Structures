3d9d2e1220a39e540a0f81a1e538d19a27af503f
Jauharuddin <jauharnotes@gmail.com>
2022-11-02 21:16:44 +0700

    first commit

---
+++ map.js
@@ -0,0 +1,14 @@
+const map = new Map([
+  ['a', 1],
+  ['b', 2],
+]);
+
+map.set('c', 3);
+console.log(map.has('a'));
+console.log(map.has('d'));
+console.log(map.size);
+map.clear();
+
+for (const [key, value] of map) {
+  console.log(`${key} : ${value}`);
+}
